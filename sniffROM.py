ce2232aa40b5898d2f0b30cd59d8b950427e99cc
alainiamburg <alainiamburg@gmail.com>
2017-03-17 01:33:02 -0700

    refactoring and removing whitespace

--- sniffROM.py
+++ sniffROM.py
@@ -6,12 +6,9 @@ import argparse, csv, sys
 # 0.21347662,         0, 0x05, 0xFF
 
 
-
-
 FLASH_PADDED_SIZE = 20000000     # hacky flash image start size, make this better. auto detect chip size (via JEDEC ID) and adjust accordingly?
 FLASH_FILL_BYTE = 0xFF
 FLASH_ENDING_SIZE = FLASH_WRITES_ENDING_SIZE = FLASH_PADDED_SIZE
-
 
 commands = {
 	0x01:"Write Status Register",
@@ -49,7 +46,6 @@ command_stats = {
 	0x9F:0,
 	0xAB:0}	
 
-
 def dump(data, length, addr):
 	hex = lambda line: ' '.join('{:02x}'.format(b) for b in map(ord, line))
 	str = lambda line: ''.join(31 < c < 127 and chr(c) or '.' for c in map(ord, line))
@@ -76,8 +72,6 @@ def print_data(offset):
 	dump(str(flash_image[address:address+offset]), 16, address)
 						
 
-
-
 parser = argparse.ArgumentParser(description="sniffROM - Reconstructs flash memory contents from passively captured READ/WRITE commands in a Saleae logic analyzer exported capture file. Currently supports SPI flash chips.")
 parser.add_argument("input_file", help="Saleae Logic SPI Analyzer Export File (.csv)")
 parser.add_argument("--addrlen", type=int, choices=[2,3], nargs="?", default=3, help="Length of address in bytes (default is 3)")
@@ -99,29 +93,21 @@ unknown_commands = 0
 
 
 with open(args.input_file, 'rb') as infile:
-	reader = csv.reader(infile)
-	for row in reader:
-		#print row
-		#print '{0} {1} {2} {3}'.format(row[0], row[1], row[2], row[3])
-		if row[1].isdigit():   # ignores the first header line
-			packet_time = float(row[0])
-			new_packet_id = int(row[1],16)
-			mosi_data = int(row[2], 16)
-			miso_data = int(row[3], 16)
+	packets = csv.reader(infile)
+	for packet in packets:
+		if packet[1].isdigit():                # ignores the first header line
+			packet_time = float(packet[0])
+			new_packet_id = int(packet[1],16)
+			mosi_data = int(packet[2], 16)
+			miso_data = int(packet[3], 16)
 			
-			if new_packet_id > packet_id:    # IF WE GOT A NEW COMMAND INSTANCE (new Packet ID according to Saleae SPI analyzer)
-				#print 'old pkt id was {0} and new pkt id is {1}'.format(packet_id, new_packet_id)
+			if new_packet_id > packet_id:      # IF WE GOT A NEW COMMAND INSTANCE (new Packet ID according to Saleae SPI analyzer)
 				packet_id = new_packet_id
 				new_command = mosi_data
-				
-				
-				# the new packet ID tells us the last command is finished, so dump remaining data from last command, if any
-				if offset > 0:         # 
+				if offset > 0:                 # the new packet ID tells us the last command is finished, so dump remaining data from last command, if any
 					if args.v > 1:
 						print_data(offset)
 						offset = 0
-				
-
 				if not (new_command in commands):
 					unknown_commands += 1
 					if args.v > 1:
@@ -131,24 +117,23 @@ with open(args.input_file, 'rb') as infile:
 					command_stats[command] += 1
 					if args.v > 0:
 						print 'Time: {0:.8f}   Packet ID: {1:5}  Command: 0x{2:02x} - {3}'.format(packet_time, packet_id, command, commands[command])
-				
-				if new_command == 0x03:   # New Read command, so reset address and offset in prep for incoming data
+				if new_command == 0x03:        # New Read command, so reset address and offset in prep for incoming data
 					curr_addr_byte = 0
 					address_bytes = bytearray([0x00] * args.addrlen)
 					offset = 0
-				elif new_command == 0x02:  #new page program command
+				elif new_command == 0x02:      # new page program command
 					curr_addr_byte = 0
 					address_bytes = bytearray([0x00] * args.addrlen)
 					offset = 0
-				elif new_command == 0x9f:   # Read ID command
+				elif new_command == 0x9f:      # Read ID command
 					jedec_id = bytearray([0x00] * 5)   # from 3 to 5
 					curr_byte = 0
-						
-			elif command == 0x03:        # We are in the middle of a Read command (currently receiving data)
-				read_data = miso_data    # the data in a read command comes on MISO
+
+			elif command == 0x03:              # We are in the middle of a Read command (currently receiving data)
+				read_data = miso_data          # the data in a read command comes on MISO
 				addr_data = mosi_data
 				if (args.filter == 'r' or args.filter == 'rw'):
-					if curr_addr_byte == args.addrlen:  # we have the whole address. read data starting with this row
+					if curr_addr_byte == args.addrlen:  # we have the whole address. read data starting with this packet
 						if args.endian == "msb":   # TODO add if else for different address byte lengths
 							address = (address_bytes[0] << 16) + (address_bytes[1] << 8) + (address_bytes[2] << 0)
 						elif args.endian == "lsb":
@@ -160,14 +145,13 @@ with open(args.input_file, 'rb') as infile:
 						offset += 1
 						bytes_sniffed += 1
 					else:   # get the address
-						#print 'curr_addr_byte is {0}'.format(curr_addr_byte)
 						address_bytes[curr_addr_byte] = addr_data
 						curr_addr_byte += 1
-			elif command == 0x02:	      # we are in a page (write) command
+			elif command == 0x02:	      # we are in a page program (write) command
 				write_data = mosi_data    # the data and addr in a write command goes on MOSI
 				addr_data = mosi_data
 				if (args.filter == 'w' or args.filter == 'rw'):
-					if curr_addr_byte == args.addrlen:  # we have the whole address. read data starting with this row
+					if curr_addr_byte == args.addrlen:  # we have the whole address. read data starting with this packet
 						if args.endian == "msb":
 							address = (address_bytes[0] << 16) + (address_bytes[1] << 8) + (address_bytes[2] << 0)
 						elif args.endian == "lsb":
@@ -181,10 +165,9 @@ with open(args.input_file, 'rb') as infile:
 						bytes_sniffed_written += 1
 						bytes_sniffed += 1
 					else:   # get the address
-						#print 'curr_addr_byte is {0}'.format(curr_addr_byte)
 						address_bytes[curr_addr_byte] = addr_data
 						curr_addr_byte += 1	
-			elif command == 0x9f:  # read ID command
+			elif command == 0x9f:           # read ID command
 				read_data = miso_data
 				if curr_byte <= 3:
 					jedec_id[curr_byte] = read_data
@@ -194,28 +177,20 @@ with open(args.input_file, 'rb') as infile:
 						print ' [+] Manufacturer ID: {0}'.format(hex(jedec_id[0]))
 						print ' [+] Device ID: {0} {1}'.format(hex(jedec_id[1]), hex(jedec_id[2]))
 						print ' [+] Look these up here: http://www.idhw.com/textual/chip/jedec_spd_man.html'
-	
-	# this is here again to catch the very last command. otherwise we leave the for
-	# loop without having a chance to print this. kind of ugly.
-	if offset > 0:
+	if offset > 0:    # this is here again to catch the very last command. otherwise we leave the for loop without having a chance to print this. kind of ugly.
 		if args.v > 1:
 			print_data(offset)
 			offset = 0
 
-
 	print 'Finished parsing input file'
 
-			
-
-# trim extra padding bytes (might lose valid data - if so edit FLASH_FILL_BYTE)
-# this assumes last byte is a padding byte
+# trim extra padding bytes (might lose valid data - if so edit FLASH_FILL_BYTE). this assumes last byte is a padding byte
 print '\nTrimming pad bytes...\n'
 while ((flash_image[FLASH_ENDING_SIZE-1] == FLASH_FILL_BYTE) and (FLASH_ENDING_SIZE > 0)):
 	FLASH_ENDING_SIZE -= 1
 	
 while ((flash_image_fromWrites[FLASH_WRITES_ENDING_SIZE-1] == FLASH_FILL_BYTE) and (FLASH_WRITES_ENDING_SIZE > 0)):
 	FLASH_WRITES_ENDING_SIZE -= 1
-
 
 try:
 	with open(args.o, 'wb') as outfile:
@@ -225,9 +200,8 @@ try:
 except:
 	print 'Failed to write the output file'
 
-
-print 'Rebuilt image: {0} bytes (saved to {1})\nCaptured data: {2} bytes ({3:.2f}%) ({4} bytes from WRITE commands)'.format(FLASH_ENDING_SIZE, args.o, bytes_sniffed, ((bytes_sniffed / float(FLASH_ENDING_SIZE)) * 100.0), bytes_sniffed_written)
-
+print 'Rebuilt image: {0} bytes (saved to {1})\nCaptured data: {2} bytes ({3:.2f}%) ({4} bytes from WRITE commands)'.format(
+								FLASH_ENDING_SIZE, args.o, bytes_sniffed, ((bytes_sniffed / float(FLASH_ENDING_SIZE)) * 100.0), bytes_sniffed_written)
 
 if args.summary:
 	print '\nSummary:'
