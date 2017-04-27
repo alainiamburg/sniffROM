3e546f42725b43cbafb4ae1199ced7bfdbe462fc
Alain Iamburg <alainiamburg@gmail.com>
2017-04-27 00:23:09 -0700

    updates to status register parsing

--- sniffROM.py
+++ sniffROM.py
@@ -1,21 +1,19 @@
 import argparse, csv, sys
-#from matplotlib import mpl, pyplot
-#import matplotlib.ticker as ticker
 import matplotlib
 from matplotlib import pyplot
 from matplotlib import ticker
-import numpy as np
+#import numpy as np
+import datetime
 
 ### Logic export csv file formats:
 
 ## SPI ##
-#
 # Time [s],Packet ID,MOSI,MISO
 # 0.21347662,0,0x05,0xFF
 
 ## I2C ##
 # Time [s],Packet ID,Address,Data,Read/Write,ACK/NAK
-# 46.089097500000001,0,0xA0,0x00,Write,CK
+# 46.089097500000001,0,0xA0,0x00,Write,ACK
 # 46.089121259999999,0,0xA0,0x7A,Write,ACK
 # 46.089176700000003,1,0xA1,0xA5,Read,ACK
 
@@ -47,6 +45,7 @@ spi_commands = {
 	0x20: ["Sector Erase (4K)", 						"WRITE", 	0],
 	0x2B: ["Read Security Register", 					"READ", 	0],
 	0x2F: ["Program Security Register", 					"WRITE", 	0],
+	0x31: ["Write Status Register 2",					"WRITE",	0],
 	0x32: ["Page Program (Quad I/O)", 					"WRITE", 	0],
 	0x33: ["Read Status Register 3", 					"READ", 	0],
 	0x34: ["Page Program (Quad I/O, 4-byte address)", 			"WRITE", 	0],
@@ -121,7 +120,7 @@ mapping_image = bytearray([0] * FLASH_PADDED_SIZE)
 packet_id = -1
 new_packet_id = 0
 offset = 0
-bytes_sniffed = 0            # this does not count re-reads of same memory addresses
+bytes_sniffed = 0                        # this does not count re-reads of same memory addresses
 bytes_sniffed_written = 0
 unknown_commands = 0
 jedec_id = bytearray([0x00] * 5)
@@ -174,11 +173,10 @@ for packet in packets:
 					print_data(flash_image[address:address+offset], address)
 				address = address + offset
 				offset = 0
-
 			packet_id = new_packet_id
 			curr_addr_byte = 0
 		if new_command == "Write":
-			addr_byte = sdl_data      # assume writing start addr for subsequent read cmd
+			addr_byte = sdl_data         # assume writing start addr for subsequent read cmd
 			i2c_write_addr = i2c_addr
 			address_bytes[curr_addr_byte] = addr_byte
 			if curr_addr_byte == 1:
@@ -205,8 +203,8 @@ for packet in packets:
 		mosi_data = int(packet[2], 16)
 		miso_data = int(packet[3], 16)
 		if new_packet_id > packet_id:    # IF WE GOT A NEW COMMAND INSTANCE (new Packet ID according to Saleae SPI analyzer)
-			if offset > 0:               # the new packet ID tells us the last command is finished, so dump remaining data from last command, if any
-				if args.v > 1:
+			if offset > 0:               # the new packet ID tells us the last command is finished,
+				if args.v > 1:           # so dump remaining data from last command, if any
 					print_data(flash_image[address:address+offset], address)
 					offset = 0
 			packet_id = new_packet_id
@@ -227,13 +225,14 @@ for packet in packets:
 				spi_commands[command][2] += 1
 				if args.v > 0:
 					print_new_cmd(command)
-		elif (command == 0x03) or (command == 0x0b):    # Read or Fast Read command
+		elif ((command == 0x03) or       # Read
+			  (command == 0x0b)):        # Fast Read
 			if "r" in args.filter:
-				read_byte = miso_data          # the data in a read command comes on MISO
+				read_byte = miso_data    # the data in a read command comes on MISO
 				addr_byte = mosi_data
 				if curr_addr_byte == args.addrlen:  # we have the whole address. read data
 					if (command == 0x0b) and (dummy_byte_fastread == True):
-						dummy_byte_fastread = False                  # Fast Read command sends a dummy byte (8 clock cycles) after the address
+						dummy_byte_fastread = False     # Fast Read command sends a dummy byte (8 clock cycles) after the address
 					else:
 						if args.endian == "msb":   # TODO add if else for different address byte lengths
 							address = (address_bytes[0] << 16) + (address_bytes[1] << 8) + (address_bytes[2] << 0)
@@ -253,11 +252,11 @@ for packet in packets:
 				else:   # get the address
 					address_bytes[curr_addr_byte] = addr_byte
 					curr_addr_byte += 1
-		elif command == 0x02:	      # Page Program (Write) command
+		elif command == 0x02:	         # Page Program (Write)
 			if "w" in args.filter:
-				write_byte = mosi_data    # the data and addr in a write command goes on MOSI
+				write_byte = mosi_data   # the data and addr in a write command goes on MOSI
 				addr_byte = mosi_data
-				if curr_addr_byte == args.addrlen:  # we have the whole address. read data
+				if curr_addr_byte == args.addrlen:   # we have the whole address. read data
 					if args.endian == "msb":
 						address = (address_bytes[0] << 16) + (address_bytes[1] << 8) + (address_bytes[2] << 0)
 					elif args.endian == "lsb":
@@ -274,10 +273,10 @@ for packet in packets:
 					bytes_sniffed_written += 1
 					mapping_image[address+offset] = 2
 					offset += 1
-				else:   # get the address
+				else:                    # get the address
 					address_bytes[curr_addr_byte] = addr_byte
 					curr_addr_byte += 1	
-		elif command == 0xab:           # Release Power-Down / Device ID command
+		elif command == 0xab:            # Release Power-Down / Device ID
 			read_byte = miso_data
 			if dummy_bytes_rpddid == 3:    # If this command is followed by 3 dummy bytes,
 				device_id = read_byte      #  then it is a Device ID command
@@ -285,7 +284,7 @@ for packet in packets:
 					print ' [+] Device ID: {0}'.format(hex(device_id))
 			else:
 				dummy_bytes_rpddid += 1
-		elif command == 0x9f:           # read JEDEC ID command (1 byte MFG ID, and 1-3 byte Device ID)
+		elif command == 0x9f:            # read JEDEC ID (1 byte MFG ID, and 1-3 byte Device ID)
 			read_byte = miso_data
 			if curr_id_byte <= 3:
 				jedec_id[curr_id_byte] = read_byte
@@ -294,21 +293,123 @@ for packet in packets:
 				if args.v > 0:
 					print ' [+] Manufacturer ID: {0}'.format(hex(jedec_id[0]))
 					print ' [+] Device ID: {0} {1}'.format(hex(jedec_id[1]), hex(jedec_id[2]))
-if offset > 0:    # this is here again to catch the very last command. otherwise we leave the for loop without having a chance to print this. kind of ugly.
+		elif (command == 0x01 or      # Write Status Register 1
+			  command == 0x05):       # Read Status Register 1
+			write_byte = mosi_data if command == 0x01 else miso_data
+			SR1 = write_byte
+			SRP1 = 0                # TODO handle an optional Reg 2 read byte, which contains SRP1
+			SRP0 = SR1 & (1 << 7)
+			SEC = SR1 & (1 << 6)
+			TB = SR1 & (1 << 5)
+			BP2 = SR1 & (1 << 4)
+			BP1 = SR1 & (1 << 3)
+			BP0 = SR1 & (1 << 2)
+			WEL = SR1 & (1 << 1)
+			BUSY = SR1 & (1 << 0)
+			BP_SIZE = "4KB Sectors" if SEC else "64KB Blocks"
+			TOP_BOTTOM = "From Bottom of Array" if TB else "From Top of Array"
+			if SRP1:
+				if SRP0:
+					SRP = "One Time Program"
+				else:
+					SRP = "Power Supply Lock-Down"
+			else:
+				if SRP0:
+					SRP = "Hardware Controlled (/WP)"
+				else:
+					SRP = "Software Controlled (WEL)"
+
+			if args.v > 1:
+				print '  +----------------------------------------------------------+'
+				print '  |                 Status Register 1 = 0x{:02x}                 |'.format(SR1)
+				print '  +-----+------+-------------------------------+-------------+'
+				print '  | Bit | Name | Description                   | Value       |'
+				print '  +-----+------+-------------------------------+-------------+'
+				print '  |  7  | SRP0 | Status Register Protect 0     | {:d}           |'.format(1 if SRP0 else 0)
+				print '  |  6  | SEC  | Sector Protect Bit            | {:d}           |'.format(1 if SEC else 0)
+				print '  |  5  | TB   | Top/Bottom Protect Bit        | {:d}           |'.format(1 if TB else 0)
+				print '  |  4  | BP2  | Block Protect Bit 2           | {:d}           |'.format(1 if BP2 else 0)
+				print '  |  3  | BP1  | Block Protect Bit 1           | {:d}           |'.format(1 if BP1 else 0)
+				print '  |  2  | BP0  | Block Protect Bit 0           | {:d}           |'.format(1 if BP0 else 0)
+				print '  |  1  | WEL  | Write Enable Latch            | {:d}           |'.format(1 if WEL else 0)
+				print '  |  0  | BUSY | Erase/Write In Progress       | {:d}           |'.format(1 if BUSY else 0)	
+				print '  +-----+------+-------------------------------+-------------+'
+				if args.v > 2:
+					print '  +----------------------------------------------------------+'
+					print '  |       Write Protection: {0}        |'.format(SRP)
+					print '  +---------+---------------------+---------+----------------+'
+					print '  |  Block  | Addresses           | Density | Portion        |'
+					print '  +---------+---------------------+---------+----------------+'
+					if BP2:
+						if BP1:
+							if BP0:
+								print '  |   0-127  | 0x000000 - 0x7FFFFF |    8MB  | ALL            |'
+							else:
+								if TB:
+									print '  |   0-63  | 0x000000 - 0x3FFFFF |    4MB  |   Lower 1/2    |'
+								else:
+									print '  |  64-127 | 0x400000 - 0x7FFFFF |    4MB  |   Upper 1/2    |'
+						else:
+							if BP0:
+								if SEC:
+									if TB:
+										print '  |    0   | 0x000000 - 0x007FFF |   32KB  |   Lower 1/256  |'
+									else:
+										print '  |   127  | 0x7F8000 - 0x7FFFFF |   32KB  |   Upper 1/256  |'
+								else:
+									if TB:
+										print '  |   0-31  | 0x000000 - 0x1FFFFF |    2MB  |   Lower 1/4    |'
+									else:
+										print '  |  96-127 | 0x000000 - 0x7FFFFF |    2MB  |   Upper 1/4    |'
+					else:
+						if BP1:
+							if BP0:
+								if SEC:
+									if TB:
+										print '  |     0    | 0x000000 - 0x003FFF |   16KB  |  Lower 1/512   |'
+									else:
+										print '  |    127   | 0x7FC000 - 0x7FFFFF |   16KB  |  Upper 1/512   |'
+								else:
+									if TB:
+										print '  |   0-7    | 0x000000 - 0x7FFFFF |  512KB  |  Lower 1/16    |'
+									else:
+										print '  | 120-127  | 0x780000 - 0x7FFFFF |  512KB  |  Upper 1/16    |'
+							else:
+								if SEC:
+									if TB:
+										print '  |    0    | 0x000000 - 0x001FFF |    8KB  |  Lower 1/1024  |'
+									else:
+										print '  |   127   | 0x7FE000 - 0x7FFFFF |    8KB  |  Upper 1/1024  |'
+								else:
+									if TB:
+										print '  |   0-3   | 0x000000 - 0x3FFFFF |  256KB  |   Lower 1/32   |'
+									else:
+										print '  | 124-127 | 0x7C0000 - 0x7FFFFF |  256KB  |   Upper 1/32   |'
+						else:
+							if BP0:
+								if SEC:
+									if TB:
+										print '  |    0   | 0x000000 - 0x000FFF |    4KB  |  Lower 1/2048  |'
+									else:
+										print '  |   127  | 0x7FF000 - 0x7FFFFF |    4KB  |  Upper 1/2048  |'
+								else:
+									if TB:
+										print '  |   0-1   | 0x000000 - 0x1FFFFF |  128KB  |   Lower 1/64   |'
+									else:
+										print '  | 126-127 | 0x7E0000 - 0x7FFFFF |  128KB  |   Upper 1/64   |'
+							else:
+								print '  |   NONE  |        NONE         |   NONE  |      NONE      |'
+					print '  +---------+---------------------+---------+----------------+'
+if offset > 0:
 	if args.v > 1:
 		print_data(flash_image[address:address+offset], address+offset)
 		offset = 0
-
 print 'Finished parsing input file'
-
-# trim extra padding bytes (might lose valid data - if so edit FLASH_FILL_BYTE). this assumes last byte is a padding byte
-print 'Trimming pad bytes...\n'
+print 'Trimming pad bytes...\n'          # trim extra padding bytes (might lose valid data - if so edit FLASH_FILL_BYTE). this assumes last byte is a padding byte
 while ((flash_image[FLASH_ENDING_SIZE-1] == FLASH_FILL_BYTE) and (FLASH_ENDING_SIZE > 0)):
 	FLASH_ENDING_SIZE -= 1
-	
 while ((flash_image_fromWrites[FLASH_WRITES_ENDING_SIZE-1] == FLASH_FILL_BYTE) and (FLASH_WRITES_ENDING_SIZE > 0)):
 	FLASH_WRITES_ENDING_SIZE -= 1
-
 try:
 	with open(args.o, 'wb') as outfile:
 		outfile.write(flash_image[0:FLASH_ENDING_SIZE])
@@ -316,7 +417,6 @@ try:
 		outfile.write(flash_image_fromWrites[0:FLASH_WRITES_ENDING_SIZE])
 except:
 	print 'Failed to write the output file'
-
 print 'Rebuilt image: {0} bytes (saved to {1})\nCaptured data: {2} bytes ({3:.2f}%) ({4} bytes from WRITE commands)'.format(
 		FLASH_ENDING_SIZE, args.o, bytes_sniffed, ((bytes_sniffed / float(FLASH_ENDING_SIZE)) * 100.0), bytes_sniffed_written)
 
@@ -341,7 +441,6 @@ if args.summary:
 		if unknown_commands > 0:
 			print "| Unknown | {0:9} |                                                           |".format(unknown_commands)
 		print '+---------+-----------+-----------------------------------------------------------+'
-
 
 if args.graph:
 	print '\nGenerating Graph...'
@@ -369,5 +468,5 @@ if args.graph:
 	axes = pyplot.gca()
 	axes.get_xaxis().set_major_formatter(ticker.FormatStrFormatter("0x%04x"))
 	axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(plot_func))
-	pyplot.savefig('image.png')
+	pyplot.savefig('{:%Y%m%d_%H%M%S}.png'.format(datetime.datetime.now()), dpi=fig.dpi, bbox_inches='tight')
 	pyplot.show()
